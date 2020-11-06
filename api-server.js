555a144e5668f2e1735327b9ca13868e2b8425dc
ashi72 <32313598+ashi72@users.noreply.github.com>
2020-11-05 16:05:03 -0800

    11/5 fixes

--- api-server.js
+++ api-server.js
@@ -13,23 +13,38 @@ const bodyParser = require('body-parser');
 // require("dotenv").config({
 //   path: resolve(process.cwd(), "src", "server", ".env"),
 // });
-const port = process.env.PORT || 5000;
+
+//// NOTE:
+// need to login before geturl works
+
+const port = process.env.PORT || 7000;
+
+//removed .env file contents
+// REACT_APP_AUTH0_DOMAIN=dev-wj1gj-sw.us.auth0.com
+// REACT_APP_AUTH0_CLIENT_ID=oiweWO8nyuTuDXO6qJN9WlfJ6AH1EoKd
+// REACT_APP_AUDIENCE=https://lynbrook-covid19-backend-api/
+
+
 // const appOrigin = 'http://localhost:3000';
-// const audience = process.env.REACT_APP_AUDIENCE;
-// const issuer = process.env.REACT_APP_AUTH0_DOMAIN;
-
-const appOrigin = 'https://lyncovid19-frontend-vercel.vercel.app/'
-
-const audience = 'https://lynbrook-covid19-backend-api/'
-const issuer = 'https://dev-kzr1audd.us.auth0.com/'
+// // const audience = process.env.REACT_APP_AUDIENCE;
+// // const issuer = process.env.REACT_APP_AUTH0_DOMAIN;
 //
+// //const appOrigin = 'https://lyncovid19-frontend-vercel.vercel.app/'
+// const audience = 'https://local-covid-backend-api'
+// // unlocalized: const audience = 'https://lynbrook-covid19-backend-api/'
+// const issuer = 'https://dev-wj1gj-sw.us.auth0.com/'
+// //note issuer has /, audience does not.
+// const appOrigin = 'http://localhost:3000'
+//
+// const audience = 'https://lynbrook-covid19-backend-api'
+// const issuer = 'https://dev-kzr1audd.us.auth0.com/'
 
 const app = express();
 
 
-if (!issuer || !audience) {
-  throw new Error("Please make sure that .env is in place and populated");
-}
+// if (!issuer || !audience) {
+//   throw new Error("Please make sure that .env is in place and populated");
+// }
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -44,30 +59,30 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors({ origin: appOrigin }));
 connectDB();
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `${issuer}.well-known/jwks.json`,
-  }),
-
-  audience: audience,
-  issuer: issuer,
-  algorithms: ["RS256"],
-});
+// const checkJwt = jwt({
+//   secret: jwksRsa.expressJwtSecret({
+//     cache: true,
+//     rateLimit: true,
+//     jwksRequestsPerMinute: 5,
+//     jwksUri: `${issuer}.well-known/jwks.json`,
+//   }),
+//
+//   audience: audience,
+//   issuer: issuer,
+//   algorithms: ["RS256"],
+// });
 
 app.use('/api/users', users);
-app.get("/api/public-message", (req, res) => {
-  res.send({
-    msg: "The API doesn't require an access token to share this message.",
-  });
-});
-
-app.get("/api/private-message", checkJwt, (req, res) => {
-  res.send({
-    msg: "The API successfully validated your access token.",
-  });
-});
+// app.get("/api/public-message", (req, res) => {
+//   res.send({
+//     msg: "The API doesn't require an access token to share this message.",
+//   });
+// });
+//
+// app.get("/api/private-message", checkJwt, (req, res) => {
+//   res.send({
+//     msg: "The API successfully validated your access token.",
+//   });
+// });
 
 app.listen(port, () => console.log(`API Server listening on port ${port}`));
